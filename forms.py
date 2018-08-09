8d07481036e26a3685ff798aad38db3f6d92c280
Haigang Liu <haigang@email.sc.edu>
2018-08-08 22:00:34 -0400

    added model to one img case

--- forms.py
+++ forms.py
@@ -5,15 +5,18 @@ from flask_wtf.file import FileAllowed
 
 class OneImageForm(FlaskForm):
     image = FileField('image',  validators = [DataRequired(), FileAllowed(['jpg', 'jpeg', 'png'])])
+    model_selection = SelectField('Model choice',
+                                  choices = [('DenseNet121', 'DenseNet'),
+                                             ('ResNet152', 'ResNet')],
+                                  validators = [DataRequired()])
     submit = SubmitField('submit')
 
 class MultipleImagesForm(FlaskForm):
     image = FileField('Zip file of images',  validators = [DataRequired(), FileAllowed(['zip'])])
 
     model_selection = SelectField('Model choice',
-                                  choices = [('Model1', 'DenseNet'),
-                                             ('Model2', 'ResNet'),
-                                             ('Model3', 'InceptionV3')],
+                                  choices = [('DenseNet121', 'DenseNet'),
+                                             ('ResNet152', 'ResNet')],
                                   validators = [DataRequired()])
 
     threshold = SelectField('Threshold',
@@ -21,6 +24,10 @@ class MultipleImagesForm(FlaskForm):
                                        ('0.2', '0.2'),
                                        ('0.3', '0.3'),
                                        ('0.4', '0.4'),
-                                       ('0.5', '0.5')],
+                                       ('0.5', '0.5'),
+                                       ('0.6', '0.6'),
+                                       ('0.7', '0.7'),
+                                       ('0.8', '0.8'),
+                                       ('0.9', '0.9')],
                             validators = [DataRequired()])
     submit = SubmitField('submit')
